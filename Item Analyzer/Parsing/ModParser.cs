b274b780f4d2cae1434aeb7dc8532f679567a848
Jan Daniel Andersen <jand187@gmail.com>
2014-03-21 11:27:14 +0100

    Added first parser tests.

---
+++ Item Analyzer/Parsing/ModParser.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Parsing
+{
+	public class ModParser
+	{
+		private readonly Dictionary<string, Func<string, string, IItemMod>> patterns;
+
+		public ModParser()
+		{
+			patterns = new Dictionary<string, Func<string, string, IItemMod>>
+			{
+				{@"^(?<value>\d+)% (?<name>.+)$", (name, value) => new PercentageIncreaseMod {Name = name, Value = value}},
+				{@"^\+(?<value>\d+) (?<name>.+)$", (name, value) => new AddedValueMod {Name = name, Value = value}},
+				{@"^\+(?<value>\d+)% (?<name>.+)$", (name, value) => new AddedPercentageMod {Name = name, Value = value}},
+			};
+		}
+
+		public IEnumerable<IItemMod> Parse(string text)
+		{
+			var lines = text.Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
+			return lines.Select(ParseLine).ToList();
+		}
+
+		public IItemMod ParseLine(string line)
+		{
+			foreach (var pattern in patterns)
+			{
+				var match = Regex.Match(line, pattern.Key);
+				if (match.Success)
+					return pattern.Value.Invoke(match.Groups["name"].Value, match.Groups["value"].Value);
+			}
+
+			return new UnknownMod();
+		}
+	}
+
+	public interface IItemMod
+	{
+		string Name { get; set; }
+		string Value { get; set; }
+	}
+
+	public class PercentageIncreaseMod : IItemMod
+	{
+		public string Name { get; set; }
+		public string Value { get; set; }
+	}
+
+	public class AddedValueMod : IItemMod
+	{
+		public string Name { get; set; }
+		public string Value { get; set; }
+	}
+
+	public class AddedPercentageMod : IItemMod
+	{
+		public string Name { get; set; }
+		public string Value { get; set; }
+	}
+
+	public class UnknownMod : IItemMod
+	{
+		public string Name
+		{
+			get { return "Unknown"; }
+			set { throw new InvalidOperationException(); }
+		}
+
+		public string Value
+		{
+			get { return "Unknown"; }
+			set { throw new InvalidOperationException(); }
+		}
+	}
+}
---
+++ Item Analyzer/ParsingTest/ModParserTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Parsing;
+using Xunit;
+
+namespace ParsingTest
+{
+	public class ModParserTest
+	{
+		[Fact]
+		public void Can_Parse_PercentageIncreaseMod()
+		{
+			const string text = "21% increased Elemental Damage with Weapons";
+
+			var result = new ModParser().ParseLine(text);
+
+			result.Name.Should().Be("increased Elemental Damage with Weapons");
+			result.Value.Should().Be("21");
+			result.GetType().Should().Be(typeof(PercentageIncreaseMod));
+		}
+
+		[Fact]
+		public void Can_Parse_AddedValueMod()
+		{
+			const string text = "+43 to Armour";
+
+			var result = new ModParser().ParseLine(text);
+
+			result.Name.Should().Be("to Armour");
+			result.Value.Should().Be("43");
+			result.GetType().Should().Be(typeof(AddedValueMod));
+		}
+
+		[Fact]
+		public void Can_Parse_AddedPercentageMod()
+		{
+			const string text = "+11% to Lightning Resistance";
+
+			var result = new ModParser().ParseLine(text);
+
+			result.Name.Should().Be("to Lightning Resistance");
+			result.Value.Should().Be("11");
+			result.GetType().Should().Be(typeof(AddedPercentageMod));
+		}
+
+		[Fact]
+		public void Parse_Differentiates_Between_Mod_Types()
+		{
+			var builder = new StringBuilder();
+			builder.AppendLine("+43 to Armour");
+			builder.AppendLine("+11% to Lightning Resistance");
+			builder.AppendLine("21% increased Elemental Damage with Weapons");
+			builder.AppendLine("+11% to Lightning Resistance");
+			builder.AppendLine("+43 to Armour");
+			builder.AppendLine("21% increased Elemental Damage with Weapons");
+
+			var result = new ModParser().Parse(builder.ToString()).ToList();
+
+			result[0].GetType().Should().Be(typeof(AddedValueMod));
+			result[1].GetType().Should().Be(typeof(AddedPercentageMod));
+			result[2].GetType().Should().Be(typeof(PercentageIncreaseMod));
+			result[3].GetType().Should().Be(typeof(AddedPercentageMod));
+			result[4].GetType().Should().Be(typeof(AddedValueMod));
+			result[5].GetType().Should().Be(typeof(PercentageIncreaseMod));
+		}
+
+	}
+}
