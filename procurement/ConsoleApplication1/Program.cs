7125fb31ef988529ab7a6f323dc6415598d8d769
Jan Daniel Andersen <jand187@gmail.com>
2014-04-04 14:15:14 +0200

    Created console app.

---
+++ procurement/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Security;
+using POEApi.Model;
+
+namespace ConsoleApplication1
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var model = new POEModel();
+
+			var password = new SecureString();
+			"prutprutlugt".ToCharArray().ToList().ForEach(password.AppendChar);
+			var authenticate = model.Authenticate("kimolsen@gmail.com", password, false, false);
+			var chars = model.GetCharacters();
+			var stash = model.GetStash(chars.First().League);
+		}
+	}
+}
