aac873f6f5b96b7d4198d4032e7755bd1e96c820
Jan Daniel Andersen <jand187@gmail.com>
2014-06-01 10:09:27 +0200

    Clean up.

--- Item Analyzer/PoeItemInfo.Website/App_Start/WebApiConfig.cs
+++ Item Analyzer/PoeItemInfo.Website/App_Start/WebApiConfig.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Net.Http.Headers;
 using System.Web.Http;
 using Newtonsoft.Json;
@@ -8,27 +6,26 @@ using Newtonsoft.Json.Converters;
 
 namespace PoeItemInfo.Website
 {
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			// Web API configuration and services
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+			// Web API routes
+			config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
 				routeTemplate: "api/{controller}/{action}/{id}",
-				defaults: new { action = "index", id = RouteParameter.Optional }
-            );
+				defaults: new {action = "index", id = RouteParameter.Optional}
+				);
 
 			var jsonSetting = new JsonSerializerSettings();
 			jsonSetting.Converters.Add(new StringEnumConverter());
 			config.Formatters.JsonFormatter.SerializerSettings = jsonSetting;
 
 			config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
-
-        }
-    }
+		}
+	}
 }
