82bcdae329abedd22de7d068782000c3bee0d6b5
Jan Daniel Andersen <jand187@gmail.com>
2014-06-02 09:57:03 +0200

    Added Character model.

--- Item Analyzer/PoeItemInfo.Data.Model/JSonProxy/Stash.cs
+++ Item Analyzer/PoeItemInfo.Data.Model/JSonProxy/Stash.cs
@@ -9,4 +9,13 @@ namespace PoeItemInfo.Data.Model.JSonProxy
 		public IEnumerable<Item> items { get; set; }
 		public IEnumerable<Tab> tabs { get; set; }
 	}
+
+	public class Character
+	{
+		public string name { get; set; }
+		public string league { get; set; }
+		public int classId { get; set; }
+		public string @class { get; set; }
+		public int level { get; set; }
+	}
 }
